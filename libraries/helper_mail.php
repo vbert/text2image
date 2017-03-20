07bf4f6ffa77457606536a61275c8e8d2da74cd5
Wojciech Sobczak <wsobczak@gmail.com>
2017-03-20 23:08:34 +0100

    vbert, pon 20 mar 23:08:34 2017 CET

---
+++ libraries/helper_mail.php
@@ -0,0 +1,59 @@
+<?php
+
+if (!defined('BASEPATH')) {
+	exit('Bezpośredni dostęp do skryptu jest niedozwolony!');
+}
+
+/**
+ * Helper Mail
+ *
+ * @author Wojciech Sobczak, wsobczak@gmail.com
+ * @copyright (c) 2016
+ */
+
+/**
+ *
+ * @param string $uri_for_active_photo
+ * @return array
+ */
+function mail_send($uri_for_active_photo) {
+	require 'PHPMailer/PHPMailerAutoload.php';
+	$mail = new PHPMailer;
+
+	//$mail->SMTPDebug = 3;
+	$mail->SetLanguage('pl');
+	$mail->CharSet = "UTF-8";
+	$mail->IsSMTP();
+	$mail->Host = 'podaj host';
+	$mail->SMTPAuth = true;
+	$mail->Username = 'podaj użytkownika';
+	$mail->Password = 'podaj hasło';
+	$mail->SMTPSecure = 'tls';
+	$mail->Port = 587;
+
+	$mail->SetFrom('adres email', 'Nazwa adresu email');
+	$mail->AddAddress('wsobczak@gmail.com');
+
+	$mail->IsHTML(true);
+	$mail->Subject = "Tytuł emaila";
+
+	$body = 'Aktywuj zdjęcie: <br><a href="%s">%s</a>';
+	$mail->Body = sprintf($body, $uri_for_active_photo, $uri_for_active_photo);
+	$mail->AltBody = "Aktywuj zdjęcie: \n\r" . $uri_for_active_photo;
+
+	if (!$mail->send()) {
+		$message = 'Message could not be sent.';
+		$message .= 'Mailer Error: ' . $mail->ErrorInfo;
+		$success = false;
+	} else {
+		$message = 'Message has been sent';
+		$success = true;
+	}
+
+	$result = [
+		'message' => $message,
+		'success' => $success
+	];
+
+	return $result;
+}
